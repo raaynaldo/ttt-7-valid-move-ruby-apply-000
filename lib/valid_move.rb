416a070d2a501df7946cdcc72493c4b415ae0064
Raynaldo Sutisna <raaynaldo@hotmail.com>
2020-08-20 04:13:59 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board,index)
-  if(board[index] != "0" && board[index] !="X")
+  if(board[index] != "0" && board[index] !="X" && board[index] && (index >= 0 && index <= 9)
     return true
   else
     return false
